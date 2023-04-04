cd25a495fef6285fbc02822a95c94e1b344a4923
Thomas <tommyvalotta@gmail.com>
2023-04-04 14:24:05 -0400

    Added functionality to webpage

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Agent_Randomizer
-Valorant agent randomizer
+Valorant agent randomizer changelog
+
+Apr 04/23: Added inputs type = image each with a picture of one of the four valorant roles. Each input acts as a filter. When one filter is clicked, it will lower the opacity of all agents not in that role. All agents are given a class corresponding with their role.
+
+Apr 01/23: Created array with all valorant agents.
+
+Used math.floor(math.random) to randomly select one agent when "Choose My Agent!" button is clicked.
+
+Added img of all agents currently available in the game.
--- script.js
+++ script.js
@@ -1,13 +1,116 @@
-const agents = ["Brimstone", "Viper", "Omen", "Killjoy", "Cypher", "Sova", "Sage", "Phoenix", "Jett", "Reyna", "Raze", "Breach", "Skye", "Yoru", "Astra", "KAY/O", "Chamber", "Neon", "Fade", "Harbor", "Gekko"];
+const valorantAgents = ["Brimstone", "Viper", "Omen", "Killjoy", "Cypher", "Sova", "Sage", "Phoenix", "Jett", "Reyna", "Raze", "Breach", "Skye", "Yoru", "Astra", "KAY/O", "Chamber", "Neon", "Fade", "Harbor", "Gekko"];
 
 function agentRandomizer(){
-    var randomAgent = agents[Math.floor(Math.random()*agents.length)];
+    var randomAgent = valorantAgents[Math.floor(Math.random()*valorantAgents.length)];
     return randomAgent;
 };
 
 const container = document.querySelector('#container');
-
+const agentSelector = document.querySelectorAll('.Agent');
 const runRandomizer = document.querySelector('#randomizer');
+
+const controllersSelected = document.querySelector('#selectControllers');
+const controllerAgents = document.querySelectorAll('.Controllers');
+
+const duelistsSelected = document.querySelector('#selectDuelists');
+const duelistAgents = document.querySelectorAll('.Duelists');
+
+const initiatorsSelected = document.querySelector('#selectInitiators');
+const initiatorAgents = document.querySelectorAll('.Initiators');
+
+const sentinelsSelected = document.querySelector('#selectSentinels');
+const sentinelAgents = document.querySelectorAll('.Sentinels');
+
 runRandomizer.addEventListener('click', ()=>{
-    console.log(agentRandomizer());   
+    console.log(agentRandomizer());
+    agentSelector.forEach((img) => {
+        img.classList.add('randomizerClicked');
+    })
+    if(controllersSelected.clicked == true){
+        controllerAgents.classList.remove('randomizerClicked')
+    };
+});
+
+controllersSelected.addEventListener('click', ()=>{
+    duelistAgents.forEach((duelistAgents)=>{
+        duelistAgents.classList.toggle('greyOut');
+    })
+});
+controllersSelected.addEventListener('click', ()=>{
+    initiatorAgents.forEach((initiatorAgents)=>{
+        initiatorAgents.classList.toggle('greyOut');
+    })
+});
+controllersSelected.addEventListener('click', ()=>{
+    sentinelAgents.forEach((sentinelAgents)=>{
+        sentinelAgents.classList.toggle('greyOut');
+    })
+});
+controllersSelected.addEventListener('click', ()=>{
+    selectDuelists.classList.toggle('greyOut');
+    selectInitiators.classList.toggle('greyOut');
+    selectSentinels.classList.toggle('greyOut');
+});
+
+duelistsSelected.addEventListener('click', ()=>{
+    controllerAgents.forEach((controllerAgents)=>{
+        controllerAgents.classList.toggle('greyOut');
+    })
+});
+duelistsSelected.addEventListener('click', ()=>{
+    initiatorAgents.forEach((initiatorAgents)=>{
+        initiatorAgents.classList.toggle('greyOut');
+    })
+});
+duelistsSelected.addEventListener('click', ()=>{
+    sentinelAgents.forEach((sentinelAgents)=>{
+        sentinelAgents.classList.toggle('greyOut');
+    })
+});
+duelistsSelected.addEventListener('click', ()=>{
+    selectControllers.classList.toggle('greyOut');
+    selectInitiators.classList.toggle('greyOut');
+    selectSentinels.classList.toggle('greyOut');
+});
+
+initiatorsSelected.addEventListener('click', ()=>{
+    duelistAgents.forEach((duelistAgents)=>{
+        duelistAgents.classList.toggle('greyOut');
+    })
+});
+initiatorsSelected.addEventListener('click', ()=>{
+    controllerAgents.forEach((controllerAgents)=>{
+        controllerAgents.classList.toggle('greyOut');
+    })
+});
+initiatorsSelected.addEventListener('click', ()=>{
+    sentinelAgents.forEach((sentinelAgents)=>{
+        sentinelAgents.classList.toggle('greyOut');
+    })
+});
+initiatorsSelected.addEventListener('click', ()=>{
+    selectDuelists.classList.toggle('greyOut');
+    selectControllers.classList.toggle('greyOut');
+    selectSentinels.classList.toggle('greyOut');
+});
+
+sentinelsSelected.addEventListener('click', ()=>{
+    duelistAgents.forEach((duelistAgents)=>{
+        duelistAgents.classList.toggle('greyOut');
+    })
+});
+sentinelsSelected.addEventListener('click', ()=>{
+    initiatorAgents.forEach((initiatorAgents)=>{
+        initiatorAgents.classList.toggle('greyOut');
+    })
+});
+sentinelsSelected.addEventListener('click', ()=>{
+    controllerAgents.forEach((controllerAgents)=>{
+        controllerAgents.classList.toggle('greyOut');
+    })
+});
+sentinelsSelected.addEventListener('click', ()=>{
+    selectDuelists.classList.toggle('greyOut');
+    selectInitiators.classList.toggle('greyOut');
+    selectControllers.classList.toggle('greyOut');
 });
